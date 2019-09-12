0d6f800df80dacbd4a4938db09267260daeb611d
João F. Dominguez <Joo F. Dominguez@DESKTOP-3QATDDV>
2019-09-12 18:59:58 -0300

    Especificando os Contratos do Repositorio e criando o jeito Certo de implementar os Contratos

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseRepositorio<TEntity> : IDisposable where TEntity : class
+    {
+        void Adicionar(TEntity tentity);
+        TEntity ObterPorId(int id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+
+
+
+
+
+
+    }
+}
---
+++ QuickBuy.Dominio/Contratos/IEnumerable.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickBuy.Dominio.Contratos
+{
+    public interface IEnumerable<TEntity> where TEntity : class
+    {
+    }
+}
