31449a72ce816d3f9ab8aec8943de705354e6ce3
João F. Dominguez <Joo F. Dominguez@DESKTOP-3QATDDV>
2019-09-12 08:47:43 -0300

    Definindo entidades do sistema

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    Public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Datapedido { get; set; }
+        public int UsuarioId { get; set; }
+
+
+        //Pedido deve ter pelo menos 1 pedido ou muitos pedidos
+        public  ICollection<ItemPedido> ItensPedido { get; set; }
+
+
+
+
+    }
+}
